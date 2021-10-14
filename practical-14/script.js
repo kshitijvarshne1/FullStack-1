f300c47fc9da3aecb79eb0a29841320629415cd9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-14 08:57:24 +0530

    final commit

--- practical-14/script.js
+++ practical-14/script.js
@@ -2,11 +2,13 @@ var x = document.getElementById("h");
 function sub() {
   var val = x.innerHTML;
   if (val - 1 >= 0) {
-    x.innerText = val - 1;
+    x.innerHTML = val - 1;
+  } else {
+    x.disabled = true;
   }
 }
 function add() {
   var val = x.innerHTML;
 
-  x.innerText = parseInt(val) + 1;
+  x.innerHTML = parseInt(val) + 1;
 }
